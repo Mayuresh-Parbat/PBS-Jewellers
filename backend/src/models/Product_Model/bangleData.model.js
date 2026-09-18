fb205ebebaedb013358ddfc8cc776aa0b097d433
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-18 21:54:23 +0530

    Add bangleData model schema for product data

---
+++ backend/src/models/Product_Model/bangleData.model.js
@@ -0,0 +1,22 @@
+import mongoose, {Schema} from "mongoose";
+
+const bangleDataSchema = new Schema(
+    {
+        ProductImages: {
+            type: Array,
+            required: true
+        },
+        ProductName: {
+            type: String,
+            required: true,
+        },
+        ProductCategory: {
+            type: String,
+            required: true,
+
+        },
+        ProductPrice: {
+            type: Number,
+            required: true
+        },
+       
