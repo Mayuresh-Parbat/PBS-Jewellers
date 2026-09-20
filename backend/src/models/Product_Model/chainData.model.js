cf99e3eaf93ac126d7bfc5fe06219a8dedc18182
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-20 20:00:11 +0530

    Create chainData.model.js for product schema

---
+++ backend/src/models/Product_Model/chainData.model.js
@@ -0,0 +1,18 @@
+import mongoose, {Schema} from "mongoose";
+
+const chainDataSchema = new Schema(
+    {
+        ProductImages: {
+            type: Array,
+            required: true
+        },
+        ProductName: {
+            type: String,
+            required: true,
+        },
+        ProductCategory: {
+            type: String,
+            required: true,
+
+        },
+      
