d7929f97cf4ca0d8b88c783bf335dc1b6a455231
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-21 19:20:50 +0530

    Add earring data schema to mongoose model

--- backend/src/models/Product_Model/earringData.model.js
+++ backend/src/models/Product_Model/earringData.model.js
@@ -1,1 +1,14 @@
 
+import mongoose, {Schema} from "mongoose";
+
+const earringDataSchema = new Schema(
+    {
+        ProductImages: {
+            type: Array,
+            required: true
+        },
+        ProductName: {
+            type: String,
+            required: true,
+        },
+       
