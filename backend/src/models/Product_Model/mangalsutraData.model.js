40b840fc4084f40c9bdab8922b94e0cfd1bb8ca4
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-22 19:50:01 +0530

    Add adminId reference to mangalsutraData model

--- backend/src/models/Product_Model/mangalsutraData.model.js
+++ backend/src/models/Product_Model/mangalsutraData.model.js
@@ -31,4 +31,12 @@ const mangalsutraDataSchema = new Schema(
             type: String,
             required: true,
         },
-       
+        adminId: {
+            type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true 
+        }, // Reference to Admin
+
+}, {timestamps: true});
+
+
+export const MangalsutraData = mongoose.model("MangalsutraData", mangalsutraDataSchema)
+
