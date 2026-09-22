de7e3e7c18d62514eb30ab9f02fcc7e7af3878e0
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-22 19:46:12 +0530

    Create mangalsutraData model schema
    Define schema for mangalsutra data with required fields.

--- backend/src/models/Product_Model/mangalsutraData.model.js
+++ backend/src/models/Product_Model/mangalsutraData.model.js
@@ -1,1 +1,18 @@
+import mongoose, {Schema} from "mongoose";
 
+const mangalsutraDataSchema = new Schema(
+    {
+        ProductImages: {
+            type: Array,
+            required: true
+        },
+        ProductName: {
+            type: String,
+            required: true,
+        },
+        ProductCategory: {
+            type: String,
+            required: true,
+
+        },
+       
