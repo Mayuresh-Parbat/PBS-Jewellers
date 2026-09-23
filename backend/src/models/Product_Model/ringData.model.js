20701c8fdc4f06047f4c2b1539f4807db96ee094
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-23 20:07:38 +0530

    Add ringDataSchema for product model
    Define ringDataSchema for product data storage

--- backend/src/models/Product_Model/ringData.model.js
+++ backend/src/models/Product_Model/ringData.model.js
@@ -1,1 +1,21 @@
-  
+import mongoose, {Schema} from "mongoose";
+
+const ringtDataSchema = new Schema(
+    {
+        ProductImages: {
+            type: Array,
+            required: true
+        },
+        ProductName: {
+            type: String,
+            required: true,
+        },
+        ProductCategory: {
+            type: String,
+            required: true,
+
+        },
+        ProductPrice: {
+            type: Number,
+            required: true
+        },
