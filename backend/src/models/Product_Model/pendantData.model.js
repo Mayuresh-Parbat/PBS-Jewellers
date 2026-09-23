ee37d08c173f886c95bc1e0bb26909a30b538ccb
Mayuresh-Parbat <mayureshparbat@gmail.com>
2026-09-23 19:55:35 +0530

    Add ProductQty, ProductDescription, ProductGender fields

--- backend/src/models/Product_Model/pendantData.model.js
+++ backend/src/models/Product_Model/pendantData.model.js
@@ -19,3 +19,23 @@ const pendantDataSchema = new Schema(
             type: Number,
             required: true
         },
+ProductQty: {
+            type: Number,
+            required: true,
+        },
+        ProductDescription: {
+            type: String,
+            required: true,
+        },
+        ProductGender: {
+            type: String,
+            required: true,
+        },
+        adminId: {
+            type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true 
+        }, // Reference to Admin
+
+}, {timestamps: true});
+
+
+export const PendantData = mongoose.model("PendantData", pendantDataSchema)
